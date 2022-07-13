1554e253100e21d8483f0775c266f0027fed3404
telolahy <telodan9@gmail.com>
2022-07-13 13:45:51 +0300

    templating index

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,39 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
-
+ 
 require __DIR__.'/auth.php';
+
+
+
+Route::get('/index', function () {
+    return view('boutiques.index');
+});
+Route::get('/about', function () {
+    return view('boutiques.about');
+});
+Route::get('/blog-detail', function () {
+    return view('boutiques.blog-detail');
+});
+Route::get('/blog', function () {
+    return view('boutiques.blog');
+});
+Route::get('/contact', function () {
+    return view('boutiques.contact');
+});
+Route::get('/home-02', function () {
+    return view('boutiques.home-02');
+});
+Route::get('/home-03', function () {
+    return view('boutiques.home-03');
+});
+Route::get('/product-detail', function () {
+    return view('boutiques.product-detail');
+});
+Route::get('/product', function () {
+    return view('boutiques.product');
+});
+Route::get('/shoping-cart', function () {
+    return view('boutiques.shoping-cart');
+});
+//FIN MENU NAVBAR
